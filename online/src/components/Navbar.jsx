8a5432bf71df89fd50d014b8e06c4806a9bb73db
Chidedede <spectraminds49@gmail.com>
2024-09-20 03:48:13 +0200

    created new components

---
+++ online/src/components/Navbar.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import Button from "@mui/material/Button";
+import { useTheme } from "@mui/material/styles";
+import { useMediaQuery } from "@mui/material";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+
+const Navbar = () => {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("md"));
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleMenuClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <AppBar position="static" color="primary">
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          IQ Whales
+        </Typography>
+        {isMobile ? (
+          <>
+            <IconButton
+              edge="end"
+              color="inherit"
+              onClick={handleMenuClick}
+              aria-label="menu"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              anchorEl={anchorEl}
+              open={Boolean(anchorEl)}
+              onClose={handleMenuClose}
+            >
+              <MenuItem onClick={handleMenuClose}>Home</MenuItem>
+              <MenuItem onClick={handleMenuClose}>About Us</MenuItem>
+              <MenuItem onClick={handleMenuClose}>Services</MenuItem>
+              <MenuItem onClick={handleMenuClose}>Contact</MenuItem>
+            </Menu>
+          </>
+        ) : (
+          <>
+            <Button color="inherit">Home</Button>
+            <Button color="inherit">About Us</Button>
+            <Button color="inherit">Services</Button>
+            <Button color="inherit">Contact</Button>
+          </>
+        )}
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Navbar;
