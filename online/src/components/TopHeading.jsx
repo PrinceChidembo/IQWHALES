38bc68c2e69eb5d70f21beb6d3092d50c8647d26
Chidedede <spectraminds49@gmail.com>
2024-09-27 19:42:29 +0200

    .

--- online/src/components/TopHeading.jsx
+++ online/src/components/TopHeading.jsx
@@ -1,11 +1,28 @@
 // Header.js
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../assets/css/TopHeading.css"; // Import the CSS for styling
 
 const TopHeading = () => {
+  const [heading, setHeading] = useState(
+    "Elevate Your Business to Greater Heights with IQwhales Analytics"
+  );
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setHeading((prevHeading) =>
+        prevHeading ===
+        "Elevate Your Business to Greater Heights with IQwhales Analytics"
+          ? "Build Your Website with Us"
+          : "Elevate Your Business to Greater Heights with IQwhales Analytics"
+      );
+    }, 5000); // Change text every 5 seconds
+
+    return () => clearInterval(interval); // Cleanup on unmount
+  }, []);
+
   return (
     <header className="header">
-      <h1>Elevate Your Business to Greater Heights with IQwhales Analytics</h1>
+      <h1 className="fade-in">{heading}</h1>
     </header>
   );
 };
